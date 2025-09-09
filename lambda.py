9897258c9435198292eb4ed39c77974f5d06365d
Cadu <cadumalmeida13@gmail.com>
2025-09-09 15:58:22 -0300

    lambda é estranho...

---
+++ lambda.py
@@ -0,0 +1,11 @@
+#pode usar ela junto com o map e varias outras funções
+'''
+lista1 = [1 , 2 , 4 ,8]
+lista2 = map(lambda x: x * 2,lista1)
+print(list(lista2))
+'''
+#o lambda cria funções para serem usadas de forma simples e podem ser postas em qualquer lugar
+
+teste1 = lambda x: x ** 2
+print(teste1(4))
+
