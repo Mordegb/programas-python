c0d3497135a5362a0ca1cd377835b837dd87cf61
Cadu <cadumalmeida13@gmail.com>
2025-09-09 21:57:18 -0300

    so pra eu resolver questão

--- beecround.py
+++ beecround.py
@@ -1,2 +1,8 @@
-teste = lambda x: x * 2
-print(teste(2))
+produto = {
+    "blusa": 59.90,
+    "tenis": 30.00,
+    "calça": 99.00
+}
+
+for item in sum(produto.items()):
+    print(f'produto:{item}')
