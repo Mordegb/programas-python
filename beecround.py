09fcaa10ef8fdbb4e17f43e8f547a91a3ba8c21f
Cadu <cadumalmeida13@gmail.com>
2025-09-16 14:33:25 -0300

    dicionario atualizado

--- beecround.py
+++ beecround.py
@@ -3,8 +3,9 @@ produto = {
     "tenis": 30.00,
     "calça": 99.00
 }
+for item in produto:
+    print(f'produto: {item}')
 
-for item in sum(produto.items()):
-    print(f'produto:{item}')
-#tem que estudar mais listas
+
+#tem que estudar mais dicionarios seu animal.
 #pq não to conseguindo enviar os codigos mds
