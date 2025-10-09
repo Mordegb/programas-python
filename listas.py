e06b27c672042044a46ecaabc1402c60ae4b5500
Cadu <cadumalmeida13@gmail.com>
2025-10-09 13:05:52 -0300

    exepmlo de listas em python

---
+++ listas.py
@@ -0,0 +1,19 @@
+
+cidades = ['rio de janeiro' , 'fortaleza' , 'são paulo' , 'natal']
+
+cidades.append('santa catarina') #adiciona ao final da lista e so pega o conteudo como parametro.
+print(cidades)
+
+cidades.remove('natal') #remove um conteudo de dentro da lista passando o conteudo como parametro
+print(cidades)
+
+cidades.insert(2,'acre') #insere um contudo na lista passando como parametro o indiceque quer que o conteudo seja adiciona  o conteudo
+print(cidades)
+
+cidades.pop(2) #remove um conteudo de da lista passando como parametro o indice
+print(cidades)
+
+cidades.sort() #so caso de lista com palavras ele coloca em ordem alfabetica...
+print(cidades)
+
+
