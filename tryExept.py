a6e444e964d0a38d5c1abc8360d00d867e48da4d
Cadu <cadumalmeida13@gmail.com>
2025-10-09 14:13:42 -0300

    tratamento de exeções em python

--- tryExept.py
+++ tryExept.py
@@ -5,9 +5,17 @@ while True:
     try: #vai executar um bloco de codigo que pode ter algum erro
         num = int(input('insira um numero\n'))
         print(f'numero x 2 = {num * 2}')
-        break
+        break #é pq deu bom e pode sair do loop
 
     except ValueError: # da erro de o usuario não digitar um numero
         print('por favor, insira um numero\n')
 
   
+while True:
+    try:
+        lista = ['leticia' , 'daniel' , 'cadu']
+        nun2 = int(input('indice da lista para imprimir: '))
+        print(lista[nun2])
+        break
+    except (IndexError,ValueError): #botar entre parenteses, permite ele analisar mais de um erro
+        print("tem que ser um numero, e um indice da lista\n");
